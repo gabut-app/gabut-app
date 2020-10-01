e9e5c13646b4a1a5d2f6deb0ba2f2b097ab72660
rikyhidayat21 <rikyhidayat21@gmail.com>
2020-10-01 22:15:53 +0700

    add static cerpen on ApiController

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,6 +1,18 @@
+const axios = require('axios')
+
 class ApiController {
   static cerpen(req, res) {
-
+    axios({
+      method: 'GET',
+      url: 'http://api.fdci.se/cerpen'
+    })
+      .then(response => {
+        let cerpen = response.data
+        res.status(200).json({ cerpen })
+      })
+      .catch(err => {
+        res.status(500).json({ msg: 'invalid server error'})
+      })
   }
 
   static musics(req, res) {
