11da04735edb8f05e676b8d1ff75626090c0baf4
suryo182 <suryo182@gmail.com>
2020-10-02 00:07:26 +0700

    1. Menambahkan list untuk menampilkan video link video random music video 2. Membuat fungsi untuk fetch music

--- client/main.js
+++ client/main.js
@@ -1,1 +1,36 @@
 let baseUrl = 'http://localhost:3000';
+
+// ini untuk fetch music
+fetchRandomMusic();
+
+function fetchRandomMusic() {
+  $.ajax({
+    url: `${baseUrl}/api-musics`,
+    method: 'get',
+    headers: {
+      // belum ada token, belum di setLocalStorage
+      token: localStorage.token,
+    },
+  })
+    .done((data) => {
+      console.log(data);
+      $('#container-music').empty();
+      $('#container-music').append(`
+        <li class="media bg-white rounded p-2 shadow mt-3">
+          <img src="${data.thumbnail}" alt=""
+            class="mr-3 rounded" width="100px">
+          <div>
+            <h3>${data.artistName}</h3>
+            <a href="${data.selectedVideo}" class="btn btn-outline-primary" target="_blank">Click Me!</a>
+          </div>
+        </li>
+      `);
+    })
+    .fail((err) => {
+      Swal.fire(
+        'Display todo failed',
+        err.responseJSON.errors.join(','),
+        'error'
+      );
+    });
+}
