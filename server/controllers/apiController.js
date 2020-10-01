8ded21b0e06b436ff4ac63c8f07c0c09befc0c2a
suryo182 <suryo182@gmail.com>
2020-10-01 22:19:08 +0700

    "add login page pada index.html di client" "add api controller untuk music api"

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,16 +1,47 @@
+const axios = require('axios');
+
 class ApiController {
-  static cerpen(req, res) {
+  static cerpen(req, res) {}
 
+  static async musics(req, res) {
+    try {
+      let musician = [
+        'coldplay',
+        'dua lipa',
+        'jason mraz',
+        'black pink',
+        'lady gaga',
+        'selena gomez',
+      ];
+      let random = Math.floor(Math.random() * musician.length);
+      const response = await axios.get(
+        `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${musician[random]}`
+      );
+      const artist = response.data;
+      let id = null;
+      artist.artists.forEach((element) => {
+        if (element.idArtist) {
+          id = element.idArtist;
+        }
+      });
+      console.log(id);
+      const data = await axios.get(
+        `https://theaudiodb.com/api/v1/json/1/mvid.php?i=${id}`
+      );
+      let linkVideo = [];
+      data.data.mvids.forEach((element) => {
+        if (element.strMusicVid) {
+          linkVideo.push(element.strMusicVid);
+        }
+      });
+      console.log(linkVideo);
+      res.status(200).json({ linkVideo });
+    } catch (err) {
+      res.status(400).json({ msg: `Bad request` });
+    }
   }
 
-  static musics(req, res) {
-
-  }
-
-  static movies(req, res) {
-    
-  }
-
+  static movies(req, res) {}
 }
 
-module.exports = ApiController
+module.exports = ApiController;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,12 @@
-const router = require('express').Router()
-const apiController = require('../controllers/apiController')
-const userController = require('../controllers/userController')
+const router = require('express').Router();
+const apiController = require('../controllers/apiController');
+const userController = require('../controllers/userController');
 
+router.post('/login', userController.login);
+router.post('/register', userController.register);
 
-router.post('/login', userController.login)
-router.post('/register', userController.register)
+router.get('/api-cerpen', apiController.cerpen);
+router.get('/api-musics', apiController.musics);
+router.get('/api-movies', apiController.movies);
 
-router.get('/api-cerpen', apiController.cerpen)
-router.get('/api-musics', apiController.musics)
-router.get('/api-movies', apiController.movies)
-
-
-module.exports = router
+module.exports = router;
